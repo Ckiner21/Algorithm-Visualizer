df487a47344feb0f31f03498a31485524d22a5eb
Christian Kiner <christiankiner21@icloud.com>
2022-05-03 17:43:06 -0500

    Make main screen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from importlib.resources import path
 import sys
+from turtle import home
 from pygame.locals import QUIT, MOUSEBUTTONDOWN
 import pygame
 import buttons
@@ -15,17 +17,58 @@ GREEN  = pygame.Color(0, 255, 0)
 
 
 window = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Algorithm Visualizer")
+icon = pygame.image.load("Sprites/Icon.png")
+pygame.display.set_icon(icon)
 window.fill(WHITE)
 
 
 def main():
+    home_screen = pygame.Surface(window.get_size())
+    home_screen.fill(WHITE)
+
+    title = pygame.font.SysFont("palatino linotype", 56, True)
+    title = title.render("Algorithm Visualizer", True, RED)
+    title_x = center(title, home_screen)
+    
+    sort_button = buttons.Button(sprite="Sprites/Sort.png", func=sort_screen)
+    sort_x = center(sort_button.sprite, home_screen)
+    path_find = buttons.Button(sprite="Sprites/Path.png", func=path_screen)
+    path_x = center(path_find.sprite, home_screen)
+
+    home_screen.blit(title, (title_x, 50))
+    sort_button.draw(home_screen, (sort_x, 175))
+    path_find.draw(home_screen, (path_x, 375))
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 return
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                clicked_button = buttons.clicked([sort_button, path_find], event.pos)
+                if clicked_button is not None:
+                    clicked_button.click()
 
+        window.blit(home_screen, (0,0))
         pygame.display.flip()
 
 
+def sort_screen():
+    print('sort')
+
+
+def path_screen():
+    print('path')
+
+
+def center(surf_to_draw: pygame.Surface, surf: pygame.Surface) -> int:
+    """Returns an integer representing an x coordinate that will place 
+       surf_to_draw in the center of surf if the left side of surf_to_draw is 
+       placed at x"""
+    w1 = surf.get_width()
+    w2 = surf_to_draw.get_width()
+    return (w1//2) - (w2//2)
+
+    
 main()
